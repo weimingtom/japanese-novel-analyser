457dce893809b3320c204156814783c15ad11317
Philipp Meyer <philipp.j.meyer@googlemail.com>
2012-05-11 17:19:27 +0900

    started query creator for frequency selection

--- src/database.py
+++ src/database.py
@@ -10,6 +10,9 @@ Open a existing database, or otherwise create a new
 in its place and provide access to it
 """
 class Database():
+  # string to use for accumulating functionality
+  IGNORE = 'IGNORE'
+
   def __init__(self, filename, mecab_fields):
     print('creating database')
     self.filename = filename
@@ -55,24 +58,44 @@ class Database():
   """
   Selects frequences from the database. Each parameter can be
   - a specific value: Filters results to this value
-  - be set to GROUP : Combine all words that are the same on this value
+  - be set to IGNORE: Combine all words that are the same on this value
   - be set to *     : No filtering
   Note that word can not be set to group.
   """
-  def select(self, word, poss):
+  def select(self, word, pos):
     vals = []
-    sql = 'SELECT word '
-    for i in range(len(poss)):
-      if poss[i] != 'GROUP':
+    npresent = 0
+    sql = 'SELECT sum(freq)'
+    # add displayed fields
+    if word != IGNORE:
+      npresent = npresent + 1
+      sql = sql + ', word'
+    for i in range(self.fields):
+      if poss[i] != IGNORE:
+        npresent = npresent + 1
         sql = sql + 'pos' + str(i) + ' '
-    sql = sql + 'FROM freqs WHERE '
-    if word != '*':
+    # add equality criteria
+    sql = sql + '\nFROM freqs\nWHERE '
+    if word != IGNORE and word != '*':
       sql = sql + 'word=? AND '
       vals.append(word)
-    for i in range(len(poss)):
-      if poss[i] != 'GROUP' and poss[i] != '*':
+    for i in range(self.fields):
+      if poss[i] != IGNORE and poss[i] != '*':
         sql = sql + 'pos' + str(i) + '=? AND '
-        vals.append(poss[i])
+        vals.append(pos[i])
+    # add grouping options
+    if npresent > 0:
+      sql = sql + '\nGROUP BY '
+      if word != IGNORE:
+        sql = sql + 'word'
+        npresent = npresent + 1
+        sql = sql + ', word'
+      for i in range(self.fields):
+        if poss[i] != IGNORE:
+          sql = sql + 'pos' + str(i) + ' '
+    # add ordering
+    sql + '\nORDER BY sum(freq)'
+    logger.out('executing query:\n%s' % sql)
   
   def __exit__(self, typ, value, traceback):
     self.c.close()
